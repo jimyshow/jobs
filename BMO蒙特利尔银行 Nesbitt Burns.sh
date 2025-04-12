e56a91aeba25778ed4cbb0a26ba6b14d0b114b44
jimyshow <jimyshow@gmail.com>
2025-04-12 19:23:03 +0800

    Update BMO蒙特利尔银行 Nesbitt Burns.sh

--- BMO蒙特利尔银行 Nesbitt Burns.sh
+++ BMO蒙特利尔银行 Nesbitt Burns.sh
@@ -16,18 +16,22 @@ Following AOL's acquisition of Winamp, he became AOL's youngest vice president, 
 He briefly worked at Mayfield Fund before joining Facebook in 2007.[11]
 
 While historically a Democratic Party donor,[35] contributing approximately $1.3 million over a decade,[36][37] he has recently shifted toward Republican support.[38]
-Palihapitiya 出生于斯里兰卡一个来自加勒的家庭，五岁时，他的父亲被派往渥太华的斯里兰卡高级专员公署，他搬到了加拿大。[2][3]1986 年，当他父亲的外交职务结束时，由于他父亲批评斯里兰卡内战期间对泰米尔人的暴力行为，全家在加拿大寻求庇护。[2][4]
+Palihapitiya 出生于斯里兰卡一个来自加勒的家庭，五岁时，他的父亲被派往渥太华的斯里兰卡高级专员公署，他搬到了加拿大。[2][3]1986 年，当他父亲的外交职务结束时，
+由于他父亲批评斯里兰卡内战期间对泰米尔人的暴力行为，全家在加拿大寻求庇护。[2][4]
 
-Palihapitiya 在困难的经济环境中长大，他的父亲与酗酒和失业作斗争，而他的母亲则从事家政工作。[5][6]14 岁时，帕利哈皮蒂亚开始在汉堡王工作以养家糊口。[7]他就读于 Lisgar Collegiate Institute，后来于 1999 年毕业于滑铁卢大学，获得电气工程学位。[8][9]
+Palihapitiya 在困难的经济环境中长大，他的父亲与酗酒和失业作斗争，而他的母亲则从事家政工作。[5][6]14 岁时，帕利哈皮蒂亚开始在汉堡王工作以养家糊口。[7]他就读于 Lisgar Collegiate Institute，
+后来于 1999 年毕业于滑铁卢大学，获得电气工程学位。[8][9]
 
 职业生涯
 1999-2007年：早期职业生涯
-毕业后，Palihapitiya 在 BMO Nesbitt Burns 担任衍生品交易员，然后加入加利福尼亚州的 Winamp。[10]在 AOL 收购 Winamp 后，他成为 AOL 最年轻的副总裁，并于 2004 年领导其即时通讯部门。[11][12]在 2007 年加入 Facebook 之前，他曾在梅菲尔德基金短暂工作。[11]
+毕业后，Palihapitiya 在 BMO Nesbitt Burns 担任衍生品交易员，然后加入加利福尼亚州的 Winamp。[10]在 AOL 收购 Winamp 后，他成为 AOL 最年轻的副总裁，并于 2004 年领导其即时通讯部门。[11][12]
+在 2007 年加入 Facebook 之前，他曾在梅菲尔德基金短暂工作。[11]
 
 2007-2011年：Facebook
 
 2007 年，Palihapitiya 在 Facebook 活动中发表讲话。
-在 Facebook，Palihapitiya 领导了 Facebook Beacon 的发布，这是一个失败并成为诉讼主题的广告系统。Palihapitiya 接下来专注于新用户增长;四年后，Facebook 拥有 10 亿用户。[13][14]在离开 Facebook 之前，Palihapitiya 领导了 Facebook Phone 和 Facebook Home 项目。[15][16]
+在 Facebook，Palihapitiya 领导了 Facebook Beacon 的发布，这是一个失败并成为诉讼主题的广告系统。Palihapitiya 接下来专注于新用户增长;四年后，Facebook 拥有 10 亿用户。[13][14]
+在离开 Facebook 之前，Palihapitiya 领导了 Facebook Phone 和 Facebook Home 项目。[15][16]
 
 史蒂文·利维 （Steven Levy） 在 Facebook： The Inside Story 中写道，帕利哈皮蒂亚在 Facebook 被视为“欺凌者”，[17] 他的下属经常因为他的欺凌而哭泣。[18]
 
@@ -37,7 +41,8 @@ Palihapitiya 在圣何塞的 Graphing Social Patterns 会议上发言。
 离开 Facebook 后，Palihapitiya 与当时的妻子一起创立了 Social Capital。[19][20] 该公司最初作为传统风险投资基金运营，投资于包括 Yammer（被 Microsoft 以 $1.2B 收购）、[21] 
 SecondMarket、[22] Slack（被 Salesforce 以 $27B 收购）、Swarm（被SpaceX）、Groq 和 Box。[23]到 2015 年，该基金管理着超过 11 亿美元的资产。[24][25][26]
 
-2018 年，Social Capital 发生了重大变化，从多 LP 风险投资基金转变为家族办公室结构。[27][28]从那时起，Social Capital 在三个领域进行了投资：气候科学、生命科学和生物技术，以及通过区块链、加密和数字资产等平台实现数字经济的去中心化。Palihapitiya 从永久资本的资产负债表进行管理。[29][30][31]
+2018 年，Social Capital 发生了重大变化，从多 LP 风险投资基金转变为家族办公室结构。[27][28]从那时起，Social Capital 在三个领域进行了投资：气候科学、生命科学和生物技术，
+以及通过区块链、加密和数字资产等平台实现数字经济的去中心化。Palihapitiya 从永久资本的资产负债表进行管理。[29][30][31]
 
 Palihapitiya 已成为特殊目的收购公司 （SPAC） 运动中的杰出人物。值得注意的交易包括 2019 年将维珍银河上市，[32] 2020 年公开开放，以及后来的 SoFi[33] 和 Clover Health。[34]
 
