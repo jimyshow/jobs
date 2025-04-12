bb81c3413ee6a8f0a0631c693da34ed9647dace8
jimyshow <jimyshow@gmail.com>
2025-04-12 19:20:14 +0800

    Update BMO蒙特利尔银行 Nesbitt Burns.sh

--- BMO蒙特利尔银行 Nesbitt Burns.sh
+++ BMO蒙特利尔银行 Nesbitt Burns.sh
@@ -2,15 +2,50 @@
 这家公司起源于 1901 年，并且后来与其他金融机构合并，成为加拿大皇家银行（RBC）的一部分。
 因此，“Nesbitt Burns” 并没有直接的中文含义，而是一个由创始人名字组成的品牌。
 
-如果你在问的是 “Nesbitt Burns” 这个名字的字面意思，“Nesbitt” 是个姓氏，“Burns” 也是一个常见的姓氏。
+BMO 利时证券Solomon与Steve 团队 Solomon Chin, MBA, CIM® 企管碩士，理學士，特許投資經理  
+陈润兴先生于1987年获得商业企管硕士学位后，进入投资理财服务行业至今。 “基线成 长TM”为陈润兴先生所设计的一套以高度专注与严谨之独特投资策略，
 
-BMO 利时证券Solomon与Steve 团队
-
-基线成长™ 是我们注册的商标系统，旨在实现最大投资增长，同时采用严谨的方法控制风险。我们专注于两个关键投资原则
- 
-
-资本增长
-资本保全
- 
-
+基线成长™ 是我们注册的商标系统，旨在实现最大投资增长，同时采用严谨的方法控制风险。我们专注于两个关键投资原则  资本增长  资本保全,
 我们的投资理念是每天使用名为 BaseLine Growth 的技术研究平台评估数千种股票和固定收益机会（包括货币）。我们的“首选”构成了我们一系列 BaseLine 投资组合的基础。​
+
+Chamath Palihapitiya
+He attended Lisgar Collegiate Institute and later graduated from the University of Waterloo in 1999 with a degree in electrical engineering.[8][9]
+Career 1999–2007: Early career
+After graduation, Palihapitiya worked as a derivatives trader at BMO Nesbitt Burns before joining Winamp in California.[10] 
+Following AOL's acquisition of Winamp, he became AOL's youngest vice president, heading its instant messaging division in 2004.[11][12] 
+He briefly worked at Mayfield Fund before joining Facebook in 2007.[11]
+
+While historically a Democratic Party donor,[35] contributing approximately $1.3 million over a decade,[36][37] he has recently shifted toward Republican support.[38]
+Palihapitiya 出生于斯里兰卡一个来自加勒的家庭，五岁时，他的父亲被派往渥太华的斯里兰卡高级专员公署，他搬到了加拿大。[2][3]1986 年，当他父亲的外交职务结束时，由于他父亲批评斯里兰卡内战期间对泰米尔人的暴力行为，全家在加拿大寻求庇护。[2][4]
+
+Palihapitiya 在困难的经济环境中长大，他的父亲与酗酒和失业作斗争，而他的母亲则从事家政工作。[5][6]14 岁时，帕利哈皮蒂亚开始在汉堡王工作以养家糊口。[7]他就读于 Lisgar Collegiate Institute，后来于 1999 年毕业于滑铁卢大学，获得电气工程学位。[8][9]
+
+职业生涯
+1999-2007年：早期职业生涯
+毕业后，Palihapitiya 在 BMO Nesbitt Burns 担任衍生品交易员，然后加入加利福尼亚州的 Winamp。[10]在 AOL 收购 Winamp 后，他成为 AOL 最年轻的副总裁，并于 2004 年领导其即时通讯部门。[11][12]在 2007 年加入 Facebook 之前，他曾在梅菲尔德基金短暂工作。[11]
+
+2007-2011年：Facebook
+
+2007 年，Palihapitiya 在 Facebook 活动中发表讲话。
+在 Facebook，Palihapitiya 领导了 Facebook Beacon 的发布，这是一个失败并成为诉讼主题的广告系统。Palihapitiya 接下来专注于新用户增长;四年后，Facebook 拥有 10 亿用户。[13][14]在离开 Facebook 之前，Palihapitiya 领导了 Facebook Phone 和 Facebook Home 项目。[15][16]
+
+史蒂文·利维 （Steven Levy） 在 Facebook： The Inside Story 中写道，帕利哈皮蒂亚在 Facebook 被视为“欺凌者”，[17] 他的下属经常因为他的欺凌而哭泣。[18]
+
+2011 年至今：社会资本
+
+Palihapitiya 在圣何塞的 Graphing Social Patterns 会议上发言。
+离开 Facebook 后，Palihapitiya 与当时的妻子一起创立了 Social Capital。[19][20] 该公司最初作为传统风险投资基金运营，投资于包括 Yammer（被 Microsoft 以 $1.2B 收购）、[21] 
+SecondMarket、[22] Slack（被 Salesforce 以 $27B 收购）、Swarm（被SpaceX）、Groq 和 Box。[23]到 2015 年，该基金管理着超过 11 亿美元的资产。[24][25][26]
+
+2018 年，Social Capital 发生了重大变化，从多 LP 风险投资基金转变为家族办公室结构。[27][28]从那时起，Social Capital 在三个领域进行了投资：气候科学、生命科学和生物技术，以及通过区块链、加密和数字资产等平台实现数字经济的去中心化。Palihapitiya 从永久资本的资产负债表进行管理。[29][30][31]
+
+Palihapitiya 已成为特殊目的收购公司 （SPAC） 运动中的杰出人物。值得注意的交易包括 2019 年将维珍银河上市，[32] 2020 年公开开放，以及后来的 SoFi[33] 和 Clover Health。[34]
+
+政治立场和活动
+虽然历史上是民主党的捐助者，[35] 十年来捐赠了大约 130 万美元，[36][37] 但他最近转向了共和党的支持。[38]
+
+2011 年，Palihapitiya 向美国参议院候选人 Ted Cruz 捐赠了 7,500 美元。[37]2020 年 3 月，帕利哈皮蒂亚告诉《纽约时报》，
+他希望看到迈克尔·布隆伯格在 2020 年民主党总统初选中与艾米·克洛布查尔或伊丽莎白·沃伦搭档成为民主党票数第一。
+[39][40]2023 年，他为总统候选人维韦克·拉马斯瓦米 （Vivek Ramaswamy） 举办了一场每盘 50,000 美元的筹款活动，[41][42]
+2024 年，他与他的 All-In Podcast 联合主持人大卫·萨克斯 （David O. Sacks） 在旧金山共同举办了一场价值 1200 万美元的特朗普筹款活动。
+[43]他与大卫·萨克斯的妻子杰奎琳·萨克斯 （Jacqueline Sacks） 共同主持了这场筹款活动，该活动筹集了 1200 万美元，并于 2024 年 6 月 6 日在萨克斯位于太平洋高地的家中举行。[44]
